72a13c0f5782ce59bf0210129314dccc9784eb89
Ranjeet361 <ranjeet.singh361@gmail.com>
2015-02-13 16:28:39 +0530

    some update on serialier

--- inventory_management/serializers.py
+++ inventory_management/serializers.py
@@ -30,6 +30,6 @@ class InventoryProductWithImagesSerializer(serializers.ModelSerializer):
         model = InventoryProducts
         fields = ('id', 'product_sku', 'name', 'purchase_price', 'retail_price', 'tax_price', 'meta_data',
                   'category_id', 'barcode', 'stock_value', 'minimum_stock_level', 'user_id', 'status', 'created',
-                  'image')
+                  )
 
 ''' tesing close '''
