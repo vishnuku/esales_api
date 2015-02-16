ff13fb93218c1add3c646aa6f5f9496fc14d2d1c
vishnu <vishnuku@gmail.com>
2015-02-16 15:19:49 +0530

    Refactored URL

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^categories/$', views.categories, name='inventory_categories'),
-    url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='inventory_category'),
-    url(r'^inventory_products/$', views.products, name='inventory_products'),
-    url(r'^inventory_product/(?P<pk>[0-9]+)/$', views.product, name='inventory_product'),
-    url(r'^inventory_images/$', views.InventoryProductImageList.as_view(), name="inventory_images"),
-    url(r'^inventory_images/(?P<pk>[0-9]+)/$', views.InventoryProductImageDetails.as_view(), name='inventory_images'),
-    url(r'^inventory_product_images/$', views.InventoryProductWithImagesList.as_view(), name="inventory_product_images"),
-    url(r'^inventory_product_image/(?P<pk>[0-9]+)/$', views.InventoryProductWithImagesDetails.as_view(), name='inventory_product_images'),
+    url(r'^inventory/categories/$', views.categories, name='inventory_categories'),
+    url(r'^inventory/category/(?P<pk>[0-9]+)/$', views.category, name='inventory_category'),
+    url(r'^inventory/products/$', views.products, name='inventory_products'),
+    url(r'^inventory/product/(?P<pk>[0-9]+)/$', views.product, name='inventory_product'),
+    url(r'^inventory/images/$', views.InventoryProductImageList.as_view(), name="inventory_images"),
+    url(r'^inventory/image/(?P<pk>[0-9]+)/$', views.InventoryProductImageDetails.as_view(), name='inventory_images'),
+    url(r'^inventory/product/images/$', views.InventoryProductWithImagesList.as_view(), name="inventory_product_images"),
+    url(r'^inventory/product/image/(?P<pk>[0-9]+)/$', views.InventoryProductWithImagesDetails.as_view(), name='inventory_product_images'),
 ]
-
