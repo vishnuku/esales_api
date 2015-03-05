a0de2f910483f2a67de9cfbaa969d8c74f90598f
vishnu <vishnuku@gmail.com>
2015-03-05 16:46:07 +0530

    Added token regeneration on api login

--- esales_api/urls.py
+++ esales_api/urls.py
@@ -4,7 +4,7 @@ from rest_framework import serializers, viewsets, routers
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import obtain_auth_token
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -34,6 +34,7 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('integration.urls')),
     url(r'^', include('inventory.urls')),
+    url(r'^api-token-auth/', obtain_auth_token),
     # url(r'^', include('channel_integration.urls')),
     # url(r'^', include('inventory_management.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
