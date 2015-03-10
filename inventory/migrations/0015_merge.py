1dba8e8a689f81ecc67d0097e994dc3c49f1ef66
vishnu <vishnuku@gmail.com>
2015-03-10 20:30:08 +0530

    Added migrations

---
+++ inventory/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0014_merge'),
+        ('inventory', '0014_auto_20150305_1052'),
+    ]
+
+    operations = [
+    ]
