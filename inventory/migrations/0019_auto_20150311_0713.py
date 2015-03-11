bb817b8a894c1d6dfa6b4f7e505bd580f118e947
vishnu <vishnuku@gmail.com>
2015-03-11 19:20:20 +0530

    Removed unwanted applications
    Added all migration scripts

---
+++ inventory/migrations/0019_auto_20150311_0713.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0018_auto_20150311_0710'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='csv',
+            name='csv_name',
+            field=models.FileField(max_length=200, upload_to=b'/Users/vishnu/PycharmProjects/esales_api/esales_api/media/csv/%Y-%m-%d'),
+            preserve_default=True,
+        ),
+    ]
---
+++ inventory/migrations/0020_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('inventory', '0019_auto_20150311_1250'),
+        ('inventory', '0019_auto_20150311_0713'),
+    ]
+
+    operations = [
+    ]
