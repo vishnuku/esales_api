62a2ef19adc10ed70b3009f06cd01d219b8e6454
Ranjeet361 <ranjeet.singh361@gmail.com>
2015-03-20 13:58:14 +0530

    add field2 for asin in order item

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -42,7 +42,7 @@ class ProductWithImagesSerializer(serializers.ModelSerializer):
         model = Product
         fields = ('id', 'name', 'brand', 'description', 'bullet_point', 'manufacturer', 'ucodetype', 'ucodevalue',
                   'purchase_price', 'retail_price', 'tax_price', 'sku', 'barcode', 'stock_quantity', 'min_stock_quantity',
-                  'sold_quantity', 'category', 'channel', 'meta_data', 'misc_data', 'origin', 'created_on', 'images')
+                  'sold_quantity', 'category', 'channel', 'meta_data', 'misc_data', 'origin', 'created_on', 'field2', 'images')
 
 
 class UserSerializer(serializers.ModelSerializer):
