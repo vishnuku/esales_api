4de06507d2705b6d98f4186cb2d61ad3a1c10bbb
vishnu <vishnuku@gmail.com>
2015-04-17 14:50:05 +0530

    Added static root config

--- esales_api/settings.py
+++ esales_api/settings.py
@@ -100,7 +100,16 @@ TASK_SERIALIZER = "json"
 # CELERY_TASK_SERIALIZER = 'json'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
 STATIC_URL = '/static/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), STATIC_URL.strip("/"))
+
+
 MEDIA_ROOT = os.path.dirname(__file__)+'/media/'
 MEDIA_URL = '/m/'
 
