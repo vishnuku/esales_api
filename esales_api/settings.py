b9a788e72cf50ff6abf58955b52f6c67e2d64aac
vishnu <vishnuku@gmail.com>
2015-04-18 12:44:58 +0530

    Updated logger
    Added logic to update bundle_product_qty

--- esales_api/settings.py
+++ esales_api/settings.py
@@ -116,22 +116,37 @@ MEDIA_URL = '/m/'
 
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': True,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': 'debug.log',
+            'filename': 'esales.log',
+            'formatter': 'verbose'
         },
     },
     'loggers': {
-        'django.request': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
             'handlers': ['file'],
             'level': 'DEBUG',
-            'propagate': True,
         },
-    },
+    }
 }
+
 
 try:
     from settings_local import *
