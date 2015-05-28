8da6909fe6e676d4be6a85ab383c7d9136d16aad
vishnu <vishnuku@gmail.com>
2015-05-28 17:59:00 +0530

    Created task for auto mapping

--- inventory/tasks.py
+++ inventory/tasks.py
@@ -1,4 +1,5 @@
 from celery import shared_task
+from inventory.models import ProductOrder
 
 @shared_task
 def map_order_product_warehouse():
@@ -7,4 +8,10 @@ def map_order_product_warehouse():
     :return:
     :rtype:
     """
-    pass
+    for productorder in ProductOrder.objects.filter(warehousebin=None):
+        productorder_obj = ProductOrder.objects.filter(product_id=productorder.product).exclude(warehousebin=None).last()
+        if productorder_obj:
+            productorder.warehousebin = productorder_obj.warehousebin
+            productorder.save()
+            print 'Updated',productorder.id
+
