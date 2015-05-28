75cf8cdba64ef1d45f453ab64874675cd09e7b7f
vishnu <vishnuku@gmail.com>
2015-05-28 16:06:47 +0530

    Added api for shipping and packaging

--- shipping/urls.py
+++ shipping/urls.py
@@ -5,4 +5,8 @@ import views
 urlpatterns = [
     url(r'^shipping/accounts/$', views.AccountList.as_view(), name="shipping_accounts"),
     url(r'^shipping/account/(?P<pk>[0-9]+)/$', views.AccountDetails.as_view(), name='shipping_account'),
+    url(r'^shipping/services/$', views.AccountList.as_view(), name="shipping_services"),
+    url(r'^shipping/service/(?P<pk>[0-9]+)/$', views.AccountDetails.as_view(), name='shipping_service'),
+    url(r'^shipping/packagings/$', views.AccountList.as_view(), name="shipping_packagings"),
+    url(r'^shipping/packaging/(?P<pk>[0-9]+)/$', views.AccountDetails.as_view(), name='shipping_packaging'),
 ]
--- shipping/views.py
+++ shipping/views.py
@@ -2,8 +2,8 @@ from rest_framework import generics
 from rest_framework import authentication, permissions
 
 # Create your views here.
-from shipping.models import Account
-from shipping.serializers import AccountSerializer
+from shipping.models import Account, Service, Packaging
+from shipping.serializers import AccountSerializer, ServiceSerializer, PackagingSerializer
 
 
 class AccountList(generics.ListCreateAPIView):
@@ -27,4 +27,50 @@ class AccountDetails(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+
+class ServiceList(generics.ListCreateAPIView):
+    """
+    List all the categories
+    """
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated, )
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user, created_by=self.request.user.id, updated_by=self.request.user.id)
+
+
+class ServiceDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    List the Service details
+    """
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class PackagingList(generics.ListCreateAPIView):
+    """
+    List all the categories
+    """
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated, )
+    queryset = Packaging.objects.all()
+    serializer_class = PackagingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user, created_by=self.request.user.id, updated_by=self.request.user.id)
+
+
+class PackagingDetails(generics.RetrieveUpdateDestroyAPIView):
+    """
+    List the Packaging details
+    """
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Packaging.objects.all()
+    serializer_class = PackagingSerializer
 
